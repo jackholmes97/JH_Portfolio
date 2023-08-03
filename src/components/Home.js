6f1ac9ad13bc26b9490610f74a94bec7b5d19a3b
Jack Holmes <jdh97@comcast.net>
2023-08-03 13:07:18 -0700

    sizing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import Typography from "@mui/material/Typography";
 import Bounce from "react-reveal/Bounce";
 import Slide from "react-reveal/Slide";
 import Fade from "@mui/material/Fade";
-import DownloadIcon from '@mui/icons-material/Download';
 export default function Home() {
     return (
         <div className="home">
@@ -19,11 +18,11 @@ export default function Home() {
             </Box>
             <Box sx={{ width: "50vw", mt: "15vh",position: "absolute", ml: "42vh"}}>
                 <Slide left>
-                    <Typography variant="h1" gutterBottom sx={{ fontSize: "25vmin", fontFamily: "Poiret One, cursive", mb: 0, textAlign: "right", color: "rgb(27, 99, 61)", ml: "31vw" }}>JACK</Typography>
+                    <Typography variant="h1" gutterBottom sx={{fontSize: "25vmin", fontFamily: "Poiret One, cursive", mb: 0, textAlign: "right", color: "rgb(27, 99, 61)", ml: "31vw" }}>JACK</Typography>
                 </Slide>
                 <br />
                 <Slide right>
-                    <Typography variant="h1" gutterBottom className="inverted-text" sx={{ fontSize: "25vh", fontFamily: "Poiret One, cursive", mb: 0, textAlign: "right", color: "rgb(27, 99, 61)", ml: "16vw", mixBlendMode: "lighten" }}>HOLMES*</Typography>
+                    <Typography variant="h1" gutterBottom className="inverted-text" sx={{fontSize: "25vh", fontFamily: "Poiret One, cursive", mb: 0, textAlign: "right", color: "rgb(27, 99, 61)", ml: "16vw", mixBlendMode: "lighten" }}>HOLMES*</Typography>
                 </Slide>
                 <Fade in={true} timeout={8000}>
                     <Typography variant="h2" gutterBottom sx={{ml: "16vw",width: "25vw" , fontFamily: "Cardo, serif", fontStyle: "italic", textAlign: "left", fontSize: "3.5vh", color: "rgb(27, 99, 61)" }}>* Front End & UX Developer</Typography>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,10 +21,10 @@ export default function Projects() {
                         <img className="projImg" src="./dumpp.png" alt="me"></img>
                     </Grid>
                     <Grid item xs={4}>
-                        <Box sx={{ width: "36vw", pl:"18vw"}}>
+                        <Box sx={{ width: "36vw", pl:"18vw", mt: "10vh"}}>
                             <Typography variant="h2" gutterBottom sx={{ mb: 0, color: "rgb(27, 99, 61)", fontFamily: "Cormorant SC, serif", textAlign: "left", fontSize: "7vh" }}>Dump</Typography>
-                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "50vh" }} />
-                            <Typography variant="body1" sx={{ textAlign: "justify", mt: 1, fontFamily: "Cardo, serif", fontSize:"2vh" }} gutterBottom>
+                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "34vw" }} />
+                            <Typography variant="body1" sx={{ textAlign: "justify", mt: 1, fontFamily: "Cardo, serif", fontSize:"2vmin" }} gutterBottom>
                                  Content curation is everywhere. While some curation helps to organize our digital world, the danger of over curation exists. What if there was a place people could share their latest vacation photos without needing professional-level photography,  the latest filter, and a photoshopped jawline? Dump was created for just this purpose. Dump is the first iteration of an image sharing site based on the recent rise in popularity of the “photo dump” trend on sites like Instagram. If you have authentic content that is ripe for sharing, just login, share it (or dump it) here.
                                 <br/>
                                 <br/>
@@ -44,10 +44,13 @@ export default function Projects() {
                 </Fade>
                 <Fade right>
                     <Grid item xs={4}>
-                        <Box sx={{mt: "75px", ml: "5vw", mb: 0, width: "29vw", mr: "25vw" }}>
+                        <img className="projImg3" src="./bshare1.png" alt="me"/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Box sx={{mt: "75px", ml: "18vw", mb: 0, width: "29vw" }}>
                             <Typography variant="h2" gutterBottom sx={{ mb: 0, color: "rgb(27, 99, 61)", fontFamily: "Cormorant SC, serif", textAlign: "left", fontSize: "7vh" }}>BlogShare</Typography>
-                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "50vh" }} />
-                            <Typography variant="body1" sx={{ fontFamily: "Cardo, serif", textAlign: "justify", mt: 1, width: "36vw", fontSize:"2vh" }} gutterBottom>
+                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "34vw" }} />
+                            <Typography variant="body1" sx={{ fontFamily: "Cardo, serif", textAlign: "justify", mt: 1, width: "36vw", fontSize:"2vmin" }} gutterBottom>
                                 Documentation and learning resources for self-taught coders are everywhere, but aren't always accessible, digestible, or easy to find. While studying at Flatiron, we needed a quick way to find and categorize the articles our fellow students were writing so we could better understand difficult concepts through various perspectives and learn from each other. BlogShare is  meant to do just that: help students explore different perspectives on a wide variety of topics covered in coding bootcamps (specifically Flatiron School).
                                 This is a Frontend project built with React.js, React Router 6, and Material UI.
                             </Typography>
@@ -61,19 +64,17 @@ export default function Projects() {
                             </Box>
                         </Box>
                     </Grid>
-                    <Grid item xs={4}>
-                        <img className="projImg2" src="./bshare1.png" alt="me"/>
-                    </Grid>
+                    
                 </Fade>
                 <Fade left>
                     <Grid item xs={4}>
                         <img className="projImg3" src="./memeg.png" alt="me"></img>
                     </Grid>
                     <Grid item xs={4}>
-                        <Box sx={{marginLeft: "19vw", pt: "9vh", width: "29vw"}}>
+                        <Box sx={{marginLeft: "18vw", pt: "9vh", width: "29vw"}}>
                             <Typography variant="h2" gutterBottom sx={{ mb: 0, color: "rgb(27, 99, 61)", fontFamily: "Cormorant SC, serif", textAlign: "left", fontSize: "7vh" }}>Meme Generator.</Typography>
-                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "67vh" }} />
-                            <Typography variant="body1" sx={{ textAlign: "justify", fontFamily: "Cardo, serif", mt: 1, width: "35vw", fontSize: "2vh" }} gutterBottom>
+                            <Divider sx={{ background: "rgb(27, 99, 61)", width: "34vw" }} />
+                            <Typography variant="body1" sx={{ textAlign: "justify", fontFamily: "Cardo, serif", mt: 1, width: "35vw", fontSize: "2vmin" }} gutterBottom>
                             This project was born out of a desire to explore the integration of external APIs in JavaScript applications. It's a Single Page Application dedicated to sharing and generating the latest memes. Simply log in, create your meme on the fly, and send it to your friends or pin it to your favorites. This project was built with Vanilla JavaScript, HTML, and CSS plus the implementation of SMTP.js and an Elastic Email SMTP server to allow for email sending capabilities. User account information is stored as JSON data and the memes are sourced using the IMGFlip API.
 
                             </Typography>
